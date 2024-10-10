d8edc1404ed5bc858c49ab9c4aed92ca7209a628
vandenberghen <ethan.vandenberghen@student.vinci.be>
2024-10-10 16:25:20 +0200

    ex2.1: 3

--- routes/users.js
+++ routes/users.js
@@ -23,29 +23,7 @@ router.post('/login', (req, res, next) => {
     const userFound = User.find(req.body.userLogin);
     console.log("User found" + JSON.stringify(userFound));
     if (userFound) {
-        if (userFound.active == false) {
-            req.session.errors = "Compte désactivé";
-            res.redirect('/users');
-        }
-        else {
-            if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
-                console.log("password correct");
-                req.session.login = req.body.userLogin;
-                req.session.connected = true;
-                if (userFound.admin) {
-                    req.session.admin = true;
-                    res.redirect('/admin');
-                } else {
-                    req.session.admin = false;
-                    res.redirect('/members');
-                }
-            }
-            else {
-                console.log("bad password");
-                req.session.errors = "Mot de passe incorrect";
-                res.redirect('/users');
-            }
-        }
+        ManageIfUserFound(userFound, req, res);
     }
     else {
         console.log("bad user");
@@ -96,3 +74,53 @@ router.post('/add', (req, res, next) => {
 });
 
 module.exports = router;
+
+function ManageIfUserFound(userFound, req, res) {
+    if (userFound.active == false) {
+        ManageLoginIfFalse(req, res);
+    }
+    else {
+        ManageLoginIfTrue(req, userFound, res);
+    }
+}
+
+function ManageLoginIfTrue(req, userFound, res) {
+    if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
+        ManagePasswordIfItsTheSame(req, userFound, res);
+    }
+    else {
+        ManagePasswordIfItsNottheSame(req, res);
+    }
+}
+
+function ManagePasswordIfItsNottheSame(req, res) {
+    console.log("bad password");
+    req.session.errors = "Mot de passe incorrect";
+    res.redirect('/users');
+}
+
+function ManagePasswordIfItsTheSame(req, userFound, res) {
+    console.log("password correct");
+    req.session.login = req.body.userLogin;
+    req.session.connected = true;
+    if (userFound.admin) {
+        ManageIfAdmin(req, res);
+    } else {
+        ManageNotAdmin(req, res);
+    }
+}
+
+function ManageNotAdmin(req, res) {
+    req.session.admin = false;
+    res.redirect('/members');
+}
+
+function ManageIfAdmin(req, res) {
+    req.session.admin = true;
+    res.redirect('/admin');
+}
+
+function ManageLoginIfFalse(req, res) {
+    req.session.errors = "Compte désactivé";
+    res.redirect('/users');
+}
